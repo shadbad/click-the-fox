73d770a2cd5cec1504825ab892a812182c265599
Sina Shadbad <shadbad@gmail.com>
2022-10-01 16:03:12 +0100

    Update the link atom

--- src/components/atoms/link/link.tsx
+++ src/components/atoms/link/link.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import './link.scss';
 
 type LinkPropTypes = {
-    className: string,
+    className?: string,
     href: string,
     children: React.ReactNode,
     variant?: 'plain' | 'button',
